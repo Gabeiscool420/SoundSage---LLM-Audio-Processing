c3d9629ef73174fbc311a1fdf2623d8a0681c7ae
SoundSage <130602253+Gabeiscool420@users.noreply.github.com>
2023-06-27 15:14:52 -0700

    Update chatbot_code_writer.py
    updated pseudocode

--- SoundSage-LLM Integration/SoundSage/chatbot_code_writer.py
+++ SoundSage-LLM Integration/SoundSage/chatbot_code_writer.py
@@ -4,6 +4,21 @@ def write_code(user_input):
     # Send the user's input to the OpenAI API
     response = openai_interaction.send_input_to_openai(user_input)
 
+    # Extract key information from the response
+    processing_type = openai_interaction.extract_info_from_response(response, 'processing_type', default='default_processing_type')
+    folder_name = openai_interaction.extract_info_from_response(response, 'folder_name', default='default_folder_name')
+    file_selection = openai_interaction.extract_info_from_response(response, 'file_selection', default='all')
+    file_type = openai_interaction.extract_info_from_response(response, 'file_type', default='default_file_type')
+    directory_name = openai_interaction.extract_info_from_response(response, 'directory_name', default='default_directory_name')
+    project_type = openai_interaction.extract_info_from_response(response, 'project_type', default='default_project_type')
+
+    # Confirm or clarify information if necessary
+    if processing_type == 'default_processing_type':
+        processing_type = openai_interaction.confirm_info('processing_type', processing_type)
+    if folder_name == 'default_folder_name':
+        folder_name = openai_interaction.confirm_info('folder_name', folder_name)
+    # ... repeat for other parameters ...
+
     # Write the response from the API to a Python file
     with open("generated_code.py", "w") as file:
         file.write(response)
