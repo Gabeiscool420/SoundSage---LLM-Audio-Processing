02ca1bdca40e005f4bf4bc826fc9917f099d08cf
SoundSage <130602253+Gabeiscool420@users.noreply.github.com>
2023-06-27 15:14:24 -0700

    Update openai_interaction.py
    updated pseudocode

--- SoundSage-LLM Integration/SoundSage/openai_interaction.py
+++ SoundSage-LLM Integration/SoundSage/openai_interaction.py
@@ -14,3 +14,13 @@ def send_input_to_openai(input):
 
     # Return the response from the API
     return response.choices[0].text.strip()
+
+def extract_info_from_response(response, keyword, default_value=None):
+    # Extract the specified information from the response
+    # This is a placeholder function. You would need to implement the actual extraction logic.
+    return default_value if keyword not in response else response[keyword]
+
+def confirm_info(keyword, value):
+    # Confirm the specified information with the user
+    # This is a placeholder function. You would need to implement the actual confirmation logic.
+    return value
