d9f21dfa7a093c6b2dddc4b4347282865af77c33
SoundSage <130602253+Gabeiscool420@users.noreply.github.com>
2023-06-30 14:24:13 -0700

    Update send_button_funtionality.py

--- SoundSage-LLM Integration/SoundSage/send_button_funtionality.py
+++ SoundSage-LLM Integration/SoundSage/send_button_funtionality.py
@@ -1,11 +1,26 @@
 import openai_interaction
+import tkinter as tk
 
 def send_button_click(event, input_field, chat_window):
     # Get the user's input
     user_input = input_field.get("1.0", 'end-1c')
 
-    # Send the user's input to the OpenAI API
-    response = openai_interaction.send_input_to_openai(user_input)
+    # Clear the input field
+    input_field.delete("1.0", tk.END)
 
-    # Display the response in the chat window
-    chat_window.insert('end', '\n' + 'ChatBot: ' + response)
+    try:
+        # Send the user's input to the OpenAI API
+        response = openai_interaction.send_input_to_openai(user_input)
+
+        # Display the response in the chat window
+        chat_window.insert('end', '\n' + 'ChatBot: ' + response)
+
+        # Scroll the chat window to the bottom
+        chat_window.see(tk.END)
+    except Exception as e:
+        # Display the error in the chat window
+        chat_window.insert('end', '\n' + 'ChatBot: ' + 'An error occurred: ' + str(e))
+
+        # Scroll the chat window to the bottom
+        chat_window.see(tk.END)
+
